d9fbc185afb1422147b73f092d2ed1491513851d
Debraj Banerjee <59164634+debrajb811@users.noreply.github.com>
2021-02-09 22:19:19 +0530

    Delete connecttoaws.php

--- connecttoaws.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Include the SDK using the Composer autoloader
-require '/var/www/html/vendor/autoload.php';
-$client = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
-]);
-?>
